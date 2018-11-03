03f2c28a7eb9f6f8af1161da815fd02b084021ff
Karl Nelson Somo Ossoal <kasom@labos.polymtl.ca>
2018-11-03 11:31:04 -0400

    ajout fichiers temporaires

---
+++ TransfertPaypal.cpp
@@ -0,0 +1,30 @@
+/********************************************
+* Titre: Travail pratique #4 - transfertPaypal.cpp
+* Date: 19 octobre 2018
+* Auteur: Wassim Khene & Ryan Hardie
+*******************************************/
+
+#include "transfertPaypal.h"
+
+TransfertPaypal::TransfertPaypal(double montant, Utilisateur * expediteur, Utilisateur * receveur)
+	: Transfert(montant, expediteur, receveur)
+{
+}
+
+string TransfertPaypal::getId() const
+{
+	return id_;
+}
+
+void TransfertPaypal::setId(string id)
+{
+	id_ = id;
+}
+
+double TransfertPaypal::getFraisTransfert() const {
+
+	if (dynamic_cast<UtilisateurRegulier*>(receveur_) != nullptr) {
+
+		expediteur_->modifierBalanceFrais((0.0226*montant_) + 0.30);
+	}
+}
